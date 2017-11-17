dc4d3a1a8ca779142f1e8841cef61cb89cac03bf
James Carter <jwcart2@tycho.nsa.gov>
2017-11-17 08:09:52 -0500

    libsepol/cil: Create new keep field for type attribute sets
    Daniel Cashman <dcashman@android.com> discovered the following:
    When using cil_db multiple_decls, the different cil_attribute nodes
    all point to the same underlying cil_attribute struct.  This leads
    to problems, though, when modifying the used value in the struct.
    __cil_post_db_attr() changes the value of the field to based on
    the output of cil_typeattribute_used(), for use later in
    cil_typeattribute_to_policydb and cil_typeattribute_to_bitmap, but
    due to the multiple declarations, cil_typeattribute_used() could be
    called again by a second node.  In this second call, the value used
    is the modifed value of CIL_TRUE or CIL_FALSE, not the flags actually
    needed. This could result in the field being reset again, to an
    incorrect CIL_FALSE value.
    
    Add the field "keep" to struct cil_typeattributeset, set its value
    using cil_typeattribute_used(), and use it when determining whether
    the attribute is to be kept or if it should be expanded.
    
    Signed-off-by: James Carter <jwcart2@tycho.nsa.gov>

--- libsepol/cil/src/cil_internal.h
+++ libsepol/cil/src/cil_internal.h
@@ -526,6 +526,7 @@ struct cil_typeattribute {
 	struct cil_list *expr_list;
 	ebitmap_t *types;
 	int used;	// whether or not this attribute was used in a binary policy rule
+	int keep;
 };
 
 struct cil_typeattributeset {
